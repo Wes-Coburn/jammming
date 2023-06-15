d1d0fe918464b8de065ee3f9216ae024ef7b7b2d
weslex555 <wgcoburn@outlook.com>
2023-06-15 22:23:56 +0100

    added enzyme with --legacy-peer-deps flag

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  /*
+  it("calls componentDidMount", () => {
+    jest.spyOn(App.prototype, "componentDidMount");
+    shallow(<App />);
+    expect(App.prototype.componentDidMount.mock.calls.length).toBe(1);
+  });
+  it("contains h4", () => {
+    const wrapper = mount(<Profile user={user} />);
+    const value = wrapper.find("h4").text();
+    expect(value).toEqual("John Doe");
+  });
+
+  it("accepts user props", () => {
+    const wrapper = mount(<Profile user={user} />);
+    expect(wrapper.props().user).toEqual(user);
+  });
+  */
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
+
+configure({ adapter: new Adapter() });
+
+it("renders correctly", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
