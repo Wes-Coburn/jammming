65dff1e1ffbe9987ac97781bda797a7778662791
weslex555 <wgcoburn@outlook.com>
2023-06-16 03:12:50 +0100

    fixed dependencies, all tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { shallow } from "enzyme";
 import App from "./App";
 
 describe("<App />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const root = createRoot(div);
+    root.render(<App />);
+    root.unmount(div);
+    //const div = document.createElement("div");
+    //ReactDOM.render(<App />, div);
+    //ReactDOM.unmountComponentAtNode(div);
   });
 
   it("renders correctly", () => {
